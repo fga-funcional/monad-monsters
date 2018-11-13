8906e74eef723adaed0b396da63e3e38a019d9e6
pablodiegoss <pablodiegoss@hotmail.com>
2018-11-13 16:23:59 -0200

    Improve card onClick event

--- front/src/Update.elm
+++ front/src/Update.elm
@@ -14,9 +14,14 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         SelectCard index card ->
-            ( { model | cards = Array.set index { card | name = card.name ++ "!" } model.cards }
-            , Cmd.none
-            )
+            if String.contains "!" card.name then
+                ( { model | cards = Array.set index { card | name = String.dropRight 1 card.name } model.cards }
+                , Cmd.none
+                )
+            else
+                ( { model | cards = Array.set index { card | name = card.name ++ "!" } model.cards }
+                , Cmd.none
+                )
 
         ToggleMission ->
             if model.showMission then
